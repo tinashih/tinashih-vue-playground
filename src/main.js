5fe0a7e0d581b9e331a62d5a7016ad1c2bab8d95
Tina Shih <tinashih@exosite.com>
2018-09-22 00:01:44 +0800

    Init a website with firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import firebase from 'firebase';
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { firebaseConfig } from '../private';
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,

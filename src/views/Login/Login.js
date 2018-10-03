231f441df284cc8ef2de7739a43266daaf45febb
Tina Shih <tinashih@exosite.com>
2018-10-03 12:10:58 +0800

    Add login page ui

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,8 +1,38 @@
 export default {
-  data() {
-    return {
-      name: 'login',
-    };
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (!valid) {
+          console.log('error submit!!');
+          return false;
+        }
+
+        return console.log('submit!', this.form);
+      });
+    },
   },
   name: 'Login',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+      formValidation: {
+        email: [
+          { required: true, message: 'Please input the email!', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: 'Please input the password!', trigger: 'blur' },
+          {
+            min: 8,
+            max: 20,
+            message: 'Length should be 8 to 20',
+            trigger: 'change',
+          },
+        ],
+      },
+      title: 'LOGIN',
+    };
+  },
 };
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="login">
-    <h1>This is a {{ name }} page</h1>
+    <el-row class="logo-block" type="flex" justify="center">
+      <el-col align="middle" :offset="9" :span="6">
+        <img src="../../assets/logo.png" />
+      </el-col>
+      <el-col align="middle" :offset="9" :span="6">
+        <h1 class="title">{{ title }}</h1>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20" type="flex" justify="center">
+      <el-col :span="8" class="info-block">
+        <el-form :model="form" :rules="formValidation" ref="form">
+          <el-form-item label="Email" prop="email">
+            <el-input clearable placeholder="Email" type="email" v-model="form.email"></el-input>
+          </el-form-item>
+          <el-form-item label="Password" prop="password">
+            <el-input clearable placeholder="Password" type="password" v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="submit" type="primary" @click="submitForm('form')">Login</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import Login from './Login';
+
 export default Login;
 </script>
 

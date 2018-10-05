f6de42f440e693dd5d4cc8d9bd9962a656fae3ba
Tina Shih <tinashih@exosite.com>
2018-10-06 00:53:24 +0800

    Beautify services page

--- src/views/Services/Services.js
+++ src/views/Services/Services.js
@@ -1,7 +1,28 @@
+const food1 = require('../../assets/images/services/food_1.jpg');
+const food2 = require('../../assets/images/services/food_2.jpg');
+const food3 = require('../../assets/images/services/food_3.jpg');
+
 export default {
   name: 'services',
   data() {
     return {
+      items: [
+        {
+          description: 'Healthy & Delicious',
+          title: 'Vegetables',
+          src: food1,
+        },
+        {
+          description: 'Different flavors to make you happy',
+          title: 'Pizza',
+          src: food2,
+        },
+        {
+          description: 'Got enough fat from it!',
+          title: 'Meat',
+          src: food3,
+        },
+      ],
       title: 'Our Services',
     };
   },
--- src/views/Services/Services.vue
+++ src/views/Services/Services.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container">
-    {{ title }}
+    <h1 class="title">{{ title }}</h1>
+    <el-row align="center" class="services" type="flex" justify="center">
+      <el-col :key="index" :span="8" v-for="(item, index) in items">
+        <el-card class="service" shadow="hover">
+          <img :src="item.src" class="image">
+          <div>
+            <h3>{{ item.title }}</h3>
+            <div>
+              {{ item.description }}
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 

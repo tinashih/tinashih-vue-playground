354a6b0c22768ae13174010de2df531fd49675c7
Tina Shih <tinashih@exosite.com>
2018-10-05 13:26:31 +0800

    Beautify login page and add fake login function

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,13 +1,45 @@
 export default {
   methods: {
+    login() {
+      const validAccount = {
+        email: 'foo@bar.com',
+        password: 'p@ssw0rd',
+      };
+      const isValidEmail = this.form.email === validAccount.email;
+      const isValidPwd = this.form.password === validAccount.password;
+      if (!isValidEmail || !isValidPwd) {
+        this.loading = false;
+        return Promise.reject(new Error('Not a valid account, please check if your email or password is correct.'));
+      }
+
+      // Generated via https://jwt.io/
+      /** Payload:
+       * {
+       *   "id": "1",
+       *   "name": "John Doe",
+       *   "email": "foo@bar.com",
+       *   "iat": 1516239022
+       * }
+      * */
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYW1lIjoiSm9obiBEb2UiLCJlbWFpbCI6ImZvb0BiYXIuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.1bPFxaLEmNE6W_1BpFZiTim19g_18SIoSknzmeY4KaM';
+      this.loading = false;
+      return Promise.resolve({ token });
+    },
     submitForm(formName) {
+      this.loading = true;
       this.$refs[formName].validate((valid) => {
         if (!valid) {
-          console.log('error submit!!');
+          this.loading = false;
           return false;
         }
 
-        return console.log('submit!', this.form);
+        return this.login()
+          .then((token) => {
+            console.log('login success => ', token);
+            this.$router.push('about');
+          }, (error) => {
+            console.log(`error happened when logging in! (${error})`);
+          });
       });
     },
   },
@@ -32,6 +64,7 @@ export default {
           },
         ],
       },
+      loading: false,
       title: 'LOGIN',
     };
   },

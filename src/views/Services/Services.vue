c7607de060918240994f8bc0b9ad5b2a3522b04a
Tina Shih <tinashih@exosite.com>
2018-10-06 17:21:03 +0800

    Add sub items for each service and add transition

--- src/views/Services/Services.vue
+++ src/views/Services/Services.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <h1 class="title">{{ title }}</h1>
     <el-row align="center" class="services" type="flex" justify="center">
-      <el-col :key="index" :span="8" v-for="(item, index) in items">
-        <el-card class="service" shadow="hover">
+      <el-col :key="index" :span="8" class="card" v-for="(item, index) in items">
+        <el-card class="service front" shadow="hover">
           <img :src="item.src" class="image">
           <div>
             <h3>{{ item.title }}</h3>
@@ -11,6 +11,11 @@
               {{ item.description }}
             </div>
           </div>
+        </el-card>
+        <el-card class="service back" shadow="hover">
+          <ul>
+            <li :key="index" class="sub-items" v-for="(sub, index) in item.subItems">{{ sub }}</li>
+          </ul>
         </el-card>
       </el-col>
     </el-row>

1e08a36a91dfdd6b2094ddac35864f07ac693e23
Tina Shih <tinashih@exosite.com>
2018-10-13 00:03:49 +0800

    Add fake content for the news page

--- src/views/News/News.vue
+++ src/views/News/News.vue
@@ -2,18 +2,14 @@
   <div class="container">
     <!-- Should has CRUD if current user is an admin in the future -->
     <h1 class="title">News</h1>
-    <keep-alive>
-      <el-collapse v-model="activeName" accordion>
-        <el-collapse-item title="Consistency" name="1">
-          <div>Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used to;</div>
-          <div>Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.</div>
-        </el-collapse-item>
-        <el-collapse-item title="Feedback" name="2">
-          <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
-          <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
-        </el-collapse-item>
-      </el-collapse>
-    </keep-alive>
+    <el-collapse accordion>
+      <el-collapse-item :key="index" :name="index" v-for="(item, index) in news">
+        <template slot="title">
+          {{ item.title }}
+        </template>
+        <div>{{ item.content }}</div>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
